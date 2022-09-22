f3504a4a533ea767251401e8f249176deb7b3cb2
VictoriaSandovalToledo <110991718+VictoriaSandovalToledo@users.noreply.github.com>
2022-09-22 07:45:05 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+let temp = 0
 basic.forever(function () {
-	
+    temp = input.temperature() * 1.8 + 32
+    if (temp >= 104 && input.lightLevel() >= 130) {
+        basic.showNumber(temp)
+        basic.showNumber(input.lightLevel())
+        basic.showIcon(IconNames.Heart)
+    } else if (temp < 104 && temp >= 86 || input.lightLevel() < 130 && input.lightLevel() > 80) {
+        basic.showNumber(temp)
+        basic.showNumber(input.lightLevel())
+        basic.showIcon(IconNames.Duck)
+    } else {
+        basic.showIcon(IconNames.House)
+    }
 })
